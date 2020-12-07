616b2e78dee3043192fb8c1d4dbb384ce502083e
Monkendrick <52892395+mcompass11@users.noreply.github.com>
2020-12-07 17:31:05 -0500

    added first pokemon

---
+++ js/scripts.js
@@ -0,0 +1,1 @@
+let pokemonList = [{name:"Charizard", height: 1.7, types: ["fire", "flying"]},{name:"Dragonite", height: 2.2, types: ["dragon", "flying"]},{name:"Mewtwo", height: 2, types: [psychic]}];
