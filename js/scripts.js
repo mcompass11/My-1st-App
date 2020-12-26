aee73098f8e9a1530dee222c0d923030deff1be4
Monkendrick <52892395+mcompass11@users.noreply.github.com>
2020-12-26 11:21:42 -0500

    added loop to objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,21 @@
-let pokemonList = [{name:"Charizard", height: 1.7, types: ["fire", "flying"]},{name:"Dragonite", height: 2.2, types: ["dragon", "flying"]},{name:"Mewtwo", height: 2, types: [psychic]}];
+let pokemonList = [
+    { 
+      name:"Charizard", 
+      height: 1.7, 
+      types: ["fire", "flying"]
+      },
+    { 
+      name:"Dragonite",
+      height: 2.2,
+      types: ["dragon", "flying"]
+      },
+    {
+      name:"Mewtwo",
+      height: 2,
+      types: "psychic"
+      }
+  ];
+  
+  for(i=0; i < pokemonList.length; i++){
+    document.write("<p>" + pokemonList[i].name + " " + "(height: " +pokemonList[i].height + ")</p>")
+  };
