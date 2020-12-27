53351d273f181338596fc8811ae3db859d08d038
Monkendrick <52892395+mcompass11@users.noreply.github.com>
2020-12-26 22:49:08 -0500

    able to detect big pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,14 @@ let pokemonList = [
       types: "psychic"
       }
   ];
-  
-  for(i=0; i < pokemonList.length; i++){
+
+  //The "for" code below, allows for my array of pokemon to be looped through
+  for( let i=0; i < pokemonList.length; i++) {
+
+    //The if/else statement below sets the result to be displayed on the index page
+    if (pokemonList[i].height > 2) {
+    document.write("<p>" + pokemonList[i].name + " " + "(height: " +pokemonList[i].height + ") - Wow, that's big!</p>")
+  }else {
     document.write("<p>" + pokemonList[i].name + " " + "(height: " +pokemonList[i].height + ")</p>")
+    }
   };
