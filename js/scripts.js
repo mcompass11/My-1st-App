619fdde7eb343a8d9f2d4e093f3e060a908716fc
Monkendrick <52892395+mcompass11@users.noreply.github.com>
2021-01-10 10:37:02 -0500

    allows my pokemon to fetch from pokemon api

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,6 @@
 let pokemonRepository = (function () {
-  let pokemonList = [
-    { 
-      name: "Charizard", 
-      height: 1.7, 
-      types: ["fire", "flying"]
-      },
-    { 
-      name: "Dragonite",
-      height: 2.2,
-      types: ["dragon", "flying"]
-      },
-    {
-      name: "Mewtwo",
-      height: 2,
-      types: "psychic"
-      }
-  ];
+  let pokemonList = [];
+  let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function getAll() {
     return pokemonList;
@@ -24,10 +9,12 @@ let pokemonRepository = (function () {
   function add(item) {
     pokemonList.push(item);
   }
+//allows the addition of pokemon to list
 
   function addListItem (pokemon) {
     let pokemonList = document.querySelector(".pokemon-List");
     let listpokemon = document.createElement("li");
+    //created list of pokemon to be displayed
     let button = document.createElement("button");
     button.innerText = pokemon.name;
     button.classList.add("button_primary");
@@ -35,23 +22,62 @@ let pokemonRepository = (function () {
     pokemonList.appendChild(listpokemon);
     button.addEventListener('click', function (event) {
       showDetails(pokemon);
+      //Create a button for each pokemon, once clicked displays pokemon info in console
     })
     }
 
   function showDetails (pokemon) {
-    console.log(pokemon);
+    loadDetails(pokemon).then(function () {
+      console.log(pokemon);
+    });
   }
 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsURL: item.url
+        };
+        //loaded pokemon api
+        add(pokemon);
+        console.log(pokemon);
+        //adds the pokemon from the results to pokemonList
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+
+  function loadDetails(item) {
+    let url = item.detailsUrl;
+    return fetch(url).then(function (response) {
+      return response.json();
+    }).then(function (details) {
+      item.imageUrl = details.sprites.front_default;
+      item.height = details.height;
+      item.types = details.types;
+      //details of pokemon to be shown
+    }).catch(function (e) {
+      console.error(e);
+    });
+  }
 
   return {
-    getAll: getAll,
     add: add,
+    loadList: loadList,
+    getAll: getAll,
     addListItem: addListItem
   };
 })();
 
 console.log(pokemonRepository.getAll());
 
-pokemonRepository.getAll().forEach( function(pokemon) {
-  pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+  //data is now loaded up
+  pokemonRepository.getAll().forEach( function(pokemon) {
+    pokemonRepository.addListItem(pokemon);
+  });
 });
